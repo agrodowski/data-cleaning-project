8bd5f83b347f4c26a7191ff2b4d06788a6c02cd2
Adam Grodowski <adam.grodowski@gmail.com>
2014-05-26 01:28:49 +0200

    final version

--- run_analysis.R
+++ run_analysis.R
@@ -10,7 +10,15 @@
 #
 library(data.table)
 
-# [agr] 0. Fetching data
+readUCIHARDataset <- function(kind, names, cols) {
+  dir <- paste0("./UCI HAR Dataset/",kind)
+  dt <- data.table(read.table(paste0(dir,"/X_",kind,".txt"),colClasses=cols))
+  setnames(dt, names)
+  dt[, activity := read.table(paste0(dir,"/y_",kind,".txt"))]
+  dt[, subject := read.table(paste0(dir,"/subject_",kind,".txt"))]
+}
+
+# Fetching data
 if (!file.exists("UCI HAR Dataset.zip")) {
   cat("Dataset package not found and shall be retrieved.")
   download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","UCI HAR Dataset.zip",method="curl")
@@ -19,29 +27,27 @@ if (!file.exists("UCI HAR Dataset")) {
   cat("Extracting UCI HAR dataset package.")
   unzip("UCI HAR Dataset.zip")
 }
-# [agr] determine which features are means and standard deviations
+# determine which features are means and standard deviations
 feature <- read.table("./UCI HAR Dataset/features.txt")
-indx <- feature[grepl("mean|std",feature$V2,feature),]$V1
-# [agr] normalise names: lowercase and removal of brackets
+indx <- feature[grepl("-(mean|std)\\(\\)",feature$V2,feature),]$V1
+# normalise names: lowercase and removal of brackets
 featurenames <- gsub("[()]","",tolower(feature[indx,]$V2))
 
-# [agr] import only relevant data from test and train datasets
+# import only relevant data from test and train datasets
 cols <- rep("NULL",length(feature$V1))
 cols[indx] <- "numeric"
 
-testdt <- data.table(read.table("./UCI HAR Dataset/test/X_test.txt",colClasses=cols))
-setnames(testdt, featurenames)
-testdt[, activity := read.table("./UCI HAR Dataset/test/y_test.txt")]
-testdt[, subject := read.table("./UCI HAR Dataset/test/subject_test.txt")]
+# read only relevant variables from test and train datasets
+testdt <- readUCIHARDataset("test",featurenames,cols)
+traindt <- readUCIHARDataset("train",featurenames,cols)
 
-traindt <- data.table(read.table("./UCI HAR Dataset/train/X_train.txt",colClasses=cols))
-setnames(traindt, featurenames)
-traindt[, activity := read.table("./UCI HAR Dataset/train/y_train.txt")]
-traindt[, subject := read.table("./UCI HAR Dataset/train/subject_train.txt")]
-
-# [agr] merge test with train
+# merge test with train
 dt <- rbindlist(list(testdt,traindt))
 
-# [agr] compute tidy dataset: reduce: mean(.SD) per subject per activity
+# replace activity number with label
+activityname <- read.table("./UCI HAR Dataset/activity_labels.txt")
+dt[, activity := as.factor(activityname[activity,]$V2) ]
+
+# compute tidy dataset: reduce: mean(.SD) per subject per activity
 dtt <- dt[,lapply(.SD,mean),by=list(subject,activity)]
 write.table(dtt,"tidy.txt",row.names=FALSE)
