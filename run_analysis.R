749bff36b456b9a3318eaeb253a83f7590205497
agrodowski <adam.grodowski@gmail.com>
2016-11-12 22:51:49 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 #
-# This file ...
+# Quoting from the project assignment:
 #
 # You should create one R script called run_analysis.R that does the following:
 # - Merges the training and the test sets to create one data set.
